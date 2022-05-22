4b6c002b7e74cfa0792ba5041a1896ac6f9365b9
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-05-22 15:28:19 +0200

    Table added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
       
       <h2> Make changes here </h2>
        
